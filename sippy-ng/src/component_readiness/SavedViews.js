3d7522bf0e24c88a1b422de52471f844ff6779b6
Dennis Periquet <dperique@redhat.com>
2024-02-12 15:40:14 -0500

    initial attempt

---
+++ sippy-ng/src/component_readiness/SavedViews.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types'
+import React, { useState } from 'react'
+
+export default function SavedViews(props) {
+  const { views, onSelectView, onDeleteView } = props
+
+  const [selectedView, setSelectedView] = useState('Default')
+
+  const handleChange = (event) => {
+    const viewName = event.target.value
+    setSelectedView(viewName)
+    const view = views.find((v) => v.name === viewName)
+    onSelectView(view)
+  }
+
+  return (
+    <div>
+      <select value={selectedView} onChange={handleChange}>
+        <option value="Default">Default</option>
+        {views.map((view) => (
+          <option key={view.id} value={view.name}>
+            {view.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
+
+SavedViews.propTypes = {
+  views: PropTypes.array.isRequired,
+  onSelectView: PropTypes.func.isRequired,
+  onDeleteView: PropTypes.func.isRequired,
+}
